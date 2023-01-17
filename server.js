4ebfbd4e8e51741b3ae86dffb8d3e018f1f12900
Filipe Almeida <filipesilva504@gmail.com>
2023-01-17 17:02:10 -0300

    criação do projeto e inicialização do express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
